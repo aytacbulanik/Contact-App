6aa3804b0aa783b81fc789bc3f71bf47d64f6494
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-11-23 16:47:18 +0300

    tableview shows data

--- Contact App/ViewController.swift
+++ Contact App/ViewController.swift
@@ -16,47 +16,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        if let filePath = Bundle.main.url(forResource: "ContactsDB", withExtension: ".plist") {
-            if let contactData = try? Data(contentsOf: filePath){
-                let plistdecoder = PropertyListDecoder()
-                do {
-                    contacts = try plistdecoder.decode([Contact].self, from: contactData)
-                } catch {
-                    print(error.localizedDescription)
+        decodePlist()
+       
+    }
+
+    func decodePlist() {
+        DispatchQueue.main.async {
+            if let filePath = Bundle.main.url(forResource: "ContactsDB", withExtension: ".plist") {
+                if let contactData = try? Data(contentsOf: filePath){
+                    let plistdecoder = PropertyListDecoder()
+                    do {
+                        self.contacts = try plistdecoder.decode([Contact].self, from: contactData)
+                        self.tableView.reloadData()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
                 }
             }
         }
+        
     }
-
-
 }
 
 extension ViewController : UITableViewDelegate , UITableViewDataSource {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+    
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let contact = contacts[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath)
-        //var content = cell.defaultContentConfiguration()
-        print(contact.firstName)
+        cell.imageView?.layer.cornerRadius = 50
+        var content = cell.defaultContentConfiguration()
+       
 
         // Configure content.
-        //content.image = UIImage(named: contact.avatarName)
-        //content.text = "\(contact.firstName) \(contact.lastName)"
-
+        content.image = UIImage(named: contact.avatarName)
+        content.text = "\(contact.firstName) \(contact.lastName)"
+       
 
         // Customize appearance.
-        //content.imageProperties.tintColor = .purple
+        content.imageProperties.tintColor = .purple
 
 
-        //cell.contentConfiguration = content
+        cell.contentConfiguration = content
 
         return cell
     }
